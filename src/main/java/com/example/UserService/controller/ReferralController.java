3a33e24f570764e01ab395ddb72daeb60b86f7fd
Shubham Garg <shubham.garg@mobikwik.com>
2024-10-04 15:40:04 +0530

    adding api methods

--- src/main/java/com/example/UserService/controller/ReferralController.java
+++ src/main/java/com/example/UserService/controller/ReferralController.java
@@ -3,11 +3,7 @@ package com.example.UserService.controller;
 import com.example.UserService.response.ReferralResponse;
 import com.example.UserService.service.ReferralService;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
@@ -19,7 +15,7 @@ public class ReferralController {
         this.referralService = referralService;
     }
 
-    @RequestMapping(value = "/referral/user-details", method = RequestMethod.GET)
+    @GetMapping("/referral/user-details")
     public ReferralResponse getUserDetailsIfExist(@RequestParam String referralCode) {
         log.debug("Request received to get user details for referralCode : {}", referralCode);
         return referralService.getUserDetailsIfExist(referralCode);
--- src/main/java/com/example/UserService/controller/UserController.java
+++ src/main/java/com/example/UserService/controller/UserController.java
@@ -7,10 +7,7 @@ import com.example.UserService.response.UserServiceResponse;
 import com.example.UserService.service.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
@@ -22,21 +19,21 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping(name = "/api/v1/save/user")
+    @PostMapping(name = "/api/v1/save/user")
     public ResponseEntity<UserServiceResponse> saveUser(@RequestBody SaveUserRequest saveUserRequest) {
         log.debug("User request received : {}", saveUserRequest);
         UserServiceResponse response = userService.saveUser(saveUserRequest);
         return ResponseEntity.ok(response);
     }
 
-    @RequestMapping(name = "/api/v1/update/user")
+    @PostMapping(name = "/api/v1/update/user")
     public ResponseEntity<UserServiceResponse> updateUserDetails(@RequestBody UpdateUserRequest updateUserRequest) {
         log.debug("update user request received : {}", updateUserRequest);
         UserServiceResponse response = userService.update(updateUserRequest);
         return ResponseEntity.ok(response);
     }
 
-    @RequestMapping(name = "/api/v1/get/user")
+    @GetMapping("/api/v1/get/user")
     public ResponseEntity<UserDetails> getUserDetails(@RequestParam String email) {
         log.debug("get user request received for emailId : {}", email);
         UserDetails userDetails = userService.getUserDetails(email);
